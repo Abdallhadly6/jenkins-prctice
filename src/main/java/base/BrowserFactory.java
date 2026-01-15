36f4710163f69f62855a87509c11f3cb4894ea19
Abdallhadly6 <58601344+Abdallhadly6@users.noreply.github.com>
2026-01-15 23:56:50 +0200

    Update BrowserFactory.java

--- src/main/java/base/BrowserFactory.java
+++ src/main/java/base/BrowserFactory.java
@@ -2,13 +2,11 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.safari.SafariDriver;
 
+import org.testng.annotations.Parameters;
 import java.io.FileReader;
 import java.io.IOException;
 import java.time.Duration;
@@ -19,58 +17,33 @@ public class BrowserFactory {
     public static WebDriver driver;
     public static Properties prop;
 
-    public WebDriver getDriver() throws IOException {
-        // Load config properties
+    @Parameters({"browserType"})
+    public WebDriver getDriver(String Browser) throws IOException {
         FileReader file = new FileReader("src/main/resources/config.Properties");
         prop = new Properties();
         prop.load(file);
 
-        // Get browser type and headless mode from system properties
-        String browser = System.getProperty("browser", "chrome").toLowerCase(); // default chrome
-        boolean headless = Boolean.parseBoolean(System.getProperty("headless", "true"));
-
-        switch (browser) {
+        switch (Browser.toLowerCase()) {
             case "chrome":
-                ChromeOptions chromeOptions = new ChromeOptions();
-                if (headless) {
-                    chromeOptions.addArguments("--headless=new");
-                    chromeOptions.addArguments("--no-sandbox");
-                    chromeOptions.addArguments("--disable-dev-shm-usage");
-                    chromeOptions.addArguments("--window-size=1920,1080");
-                }
-                driver = new ChromeDriver(chromeOptions);
+                driver = new ChromeDriver();
                 break;
-
             case "firefox":
-                FirefoxOptions firefoxOptions = new FirefoxOptions();
-                if (headless) {
-                    firefoxOptions.addArguments("-headless");
-                }
-                driver = new FirefoxDriver(firefoxOptions);
+                driver = new FirefoxDriver();
                 break;
-
             case "edge":
-                EdgeOptions edgeOptions = new EdgeOptions();
-                if (headless) {
-                    edgeOptions.addArguments("--headless=new");
-                    edgeOptions.addArguments("--no-sandbox");
-                    edgeOptions.addArguments("--disable-dev-shm-usage");
-                    edgeOptions.addArguments("--window-size=1920,1080");
-                }
-                driver = new EdgeDriver(edgeOptions);
+                driver = new EdgeDriver();
                 break;
-
             case "safari":
-                driver = new SafariDriver(); // Safari doesn’t support headless
+                driver = new SafariDriver();
                 break;
-
             default:
-                throw new IllegalArgumentException("Unsupported browser: " + browser);
+                throw new IllegalArgumentException("Please add browser");
         }
 
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
-        int implicitWait = Integer.parseInt(prop.getProperty("implicitWaitInSeconds", "10"));
+
+        int implicitWait = Integer.parseInt(prop.getProperty("implicitWaitInSeconds"));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitWait));
         driver.get(prop.getProperty("url"));
 
