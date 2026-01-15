0a30477ec1bcf3702d6b5b03862a520737f2a4cf
Abdallhadly6 <58601344+Abdallhadly6@users.noreply.github.com>
2026-01-16 00:26:14 +0200

    Update EmailManager.java

--- src/main/java/utility/EmailManager.java
+++ src/main/java/utility/EmailManager.java
@@ -14,7 +14,11 @@ import javax.mail.Transport;
 public class EmailManager {
 
     public static void sendMail() throws MessagingException {
-        ZipUtil.pack(new File("test-reports\\"), new File("test-reports.zip"));
+        String folder = "test-reports-" + browserType;
+        String zipFile = "test-reports-" + browserType + ".zip";
+
+        ZipUtil.pack(new File(folder), new File(zipFile));
+        System.out.println("Created ZIP for " + browserType + ": " + zipFile);
 
         //////////////////////////////////////////////////////////////////////////////////////
         String content = "Project Test Report At  " + String.valueOf(java.time.LocalDate.now()) ;
